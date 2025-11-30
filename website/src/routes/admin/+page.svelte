2d4b8306e3f6ba807e24c2e40e172b08e92c38b6
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2025-11-30 03:59:04 +0100

    change contact and admin page

---
+++ website/src/routes/admin/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="size-full flex gap-4">
+  <Button href='promo'>
+    Promo
+  </Button>
+  <Button href='users'>
+    Users
+  </Button>
+</div>
